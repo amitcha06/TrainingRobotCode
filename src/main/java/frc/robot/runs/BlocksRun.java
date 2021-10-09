19a4c1985a20624a9f3b303707fac7053d067b28
Itamar Schwartz <Itamarschwartz10@gmail.com>
2021-10-09 17:29:21 +0300

    Runs

--- src/main/java/frc/robot/runs/BlocksRun.java
+++ src/main/java/frc/robot/runs/BlocksRun.java
@@ -5,6 +5,11 @@
 package frc.robot.runs;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.StaticJoystick;
+import frc.robot.subsystems.Blocks;
+import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.RobotState;
+import frc.robot.subsystems.SubSystemManager;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -14,77 +19,27 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class BlocksRun extends TimedRobot {
-
+  RobotState currentState;
+  SubSystemManager subSystemManager;
   /**
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
   @Override
   public void robotInit() {
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for
-   * items like diagnostics that you want ran during disabled, autonomous,
-   * teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable chooser
-   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-   * remove all of the chooser code and uncomment the getString line to get the
-   * auto name from the text box below the Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional comparisons to the
-   * switch structure below with additional strings. If using the SendableChooser
-   * make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-
-  }
-
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {
+    subSystemManager = new SubSystemManager(new Lift() , new Blocks());
+    subSystemManager.init();
+    currentState = RobotState.TRAVEL;
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+    if(StaticJoystick.joystick.getRawButton(0)) currentState = RobotState.MIDDLE;
+    if(StaticJoystick.joystick.getRawButton(1)) currentState = RobotState.INTAKE;
+    if(StaticJoystick.joystick.getRawButton(2)) currentState = RobotState.TRAVEL;
+    if(StaticJoystick.joystick.getRawButton(3)) currentState = RobotState.TOP;
+    subSystemManager.execute(currentState);
   }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-  }
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
 }
--- src/main/java/frc/robot/runs/WaterWheelRun.java
+++ src/main/java/frc/robot/runs/WaterWheelRun.java
@@ -1,87 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.runs;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.StaticJoystick;
+import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.RobotState;
+import frc.robot.subsystems.SubSystemManager;
+import frc.robot.subsystems.WaterWheel;
 
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class WaterWheelRun extends TimedRobot {
+  RobotState currentState;
+  SubSystemManager subSystemManager;
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    subSystemManager = new SubSystemManager(new Lift() , new WaterWheel());
+    subSystemManager.init();
+    currentState = RobotState.TRAVEL;
+  }
 
-    /**
-     * The VM is configured to automatically run this class, and to call the
-     * functions corresponding to each mode, as described in the TimedRobot
-     * documentation. If you change the name of this class or the package after
-     * creating this project, you must also update the build.gradle file in the
-     * project.
-     */
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    if(StaticJoystick.joystick.getRawButton(0)) currentState = RobotState.MIDDLE;
+    if(StaticJoystick.joystick.getRawButton(1)) currentState = RobotState.INTAKE;
+    if(StaticJoystick.joystick.getRawButton(2)) currentState = RobotState.TRAVEL;
+    if(StaticJoystick.joystick.getRawButton(3)) currentState = RobotState.TOP;
+    subSystemManager.execute(currentState);
+  }
 
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    @Override
-    public void robotInit() {
-    }
-
-    /**
-     * This function is called every robot packet, no matter the mode. Use this for
-     * items like diagnostics that you want ran during disabled, autonomous,
-     * teleoperated and test.
-     *
-     * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow
-     * and SmartDashboard integrated updating.
-     */
-    @Override
-    public void robotPeriodic() {
-    }
-
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different autonomous modes using the dashboard. The sendable chooser
-     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-     * remove all of the chooser code and uncomment the getString line to get the
-     * auto name from the text box below the Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure below with additional strings. If using the SendableChooser
-     * make sure to add them to the chooser code above as well.
-     */
-    @Override
-    public void autonomousInit() {
-    }
-
-    /** This function is called periodically during autonomous. */
-    @Override
-    public void autonomousPeriodic() {
-
-    }
-
-    /** This function is called once when teleop is enabled. */
-    @Override
-    public void teleopInit() {
-    }
-
-    /** This function is called periodically during operator control. */
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    /** This function is called once when the robot is disabled. */
-    @Override
-    public void disabledInit() {
-    }
-
-    /** This function is called periodically when disabled. */
-    @Override
-    public void disabledPeriodic() {
-    }
-
-    /** This function is called once when test mode is enabled. */
-    @Override
-    public void testInit() {
-    }
-
-    /** This function is called periodically during test mode. */
-    @Override
-    public void testPeriodic() {
-    }
 }
